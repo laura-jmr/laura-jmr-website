4c650708efb7b1027165126c4b7b0c05e51bc25f
Laura Jürgensmeier <laura.juergensmeier@gmail.com>
2023-08-15 22:54:13 +0200

    feat: new bg, projects layout adjusted

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -4,7 +4,7 @@ function Landing() {
   const lauraImagePath = `${process.env.PUBLIC_URL}/assets/laura.png`;
 
   return (
-    <div className="page-landing, page">
+    <div id='page-landing' className="page">
       <h1 class="page-h1">Laura Jürgensmeier</h1>
       <img src={lauraImagePath} alt="Laura" id="landing-laura-image"/>
     </div>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,14 +2,21 @@ import './Projects.css'
 
 function Projects() {
   return (
-    <div className="page-projects, page">
+    <div id='page-projects' className="page">
       <h1 className='page-h1'>Projects</h1>
       <div id="projects-container">
-        <ul id="projects-container-ul">
-          <li><h2>Bachelor Thesis</h2></li>
-          <li><h2>Personal Website</h2></li>
+        <ul id="projects-container-nav-ul">
+          <li className='projects-container-nav-li-non-focus'><h2>Bachelor Thesis</h2></li>
+          <li id='projects-container-nav-li-focus'><h2>Personal Website</h2></li>
+          <li className='projects-container-nav-li-non-focus'><h2>Odoo Reports</h2></li>
         </ul>
-        <h2>Bachelor Thesis</h2>
+        <h2>Personal Website</h2>
+        <ul id='projects-container-info-ul'>
+          <li>Created with:</li>
+          <li>Time Span:</li>
+          <li>Status:</li>
+          <li>Contributors:</li>
+        </ul>
         <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
       </div>
     </div>
